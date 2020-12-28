a972b6277e2f9cfd98dbaf8cb4daedf22f531f3c
cthorne <cfthorne83@gmail.com>
2020-12-28 15:16:52 -0500

    finish arg exercise

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,9 +1,67 @@
-const sum = function(...args){
-    let summed = 0;
-    for(let i = 0; i < args.length; i++ ){
-       summed += args[i];
-    }
-    return summed;
+// const sum = function(...args){
+//     let summed = 0;
+//     for(let i = 0; i < args.length; i++){
+//        summed += args[i];
+//     }
+//     return summed;
+// }
+
+// Function.prototype.myBind = function(ctx){
+//     const fn = this;
+//     const bindArgs = Array.from(arguments)
+
+//     return function(){
+//         const callArgs = Array.from(arguments)
+//         return fn.apply(ctx, bindArgs.slice(1).concat(callArgs))
+//     }
+// }
+
+// Function.prototype.myBind = function(ctx, ...bindArgs){
+//     const fn = this;
+
+//     return function(...callArgs){
+//         return fn.apply(ctx, bindArgs.concat(callArgs))
+//     }
+// }
+
+const curriedSum = function(numArgs){
+    const numbers = [];
+
+        return function _curriedAdd(num){
+            numbers.push(num);
+                if (numbers.length === numArgs) {
+                    return console.log(numbers.reduce((acc, el) => acc + el));
+                } else {
+                    return _curriedAdd;
+                }
+        };
 }
 
-Function.prototype.myBind
+// Function.prototype.curry = function(numArgs){
+//     const that = this;
+//     const args = [];
+
+//     return function _func(arg){
+//         args.push(arg);
+//             if (args.length === numArgs){
+//                 return that(...args);
+//             } else {
+//                 return _func;
+//             }
+//     }
+// }
+
+Function.prototype.curry = function (numArgs) {
+    const that = this;
+    const args = [];
+
+    return function _func(arg) {
+        args.push(arg);
+        if (args.length === numArgs) {
+            return that.apply(null, args);
+        } else {
+            return _func;
+        }
+    }
+}
+    
